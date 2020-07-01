d637c5f643338ffac65dbcf47a92d65bd80d547a
Arberijeb <arberije.burrniku@studentet.uni-pr.edu>
2020-07-01 19:07:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/chatroom/model/Room.java
@@ -0,0 +1,44 @@
+package com.example.chatroom.model;
+
+import java.io.Serializable;
+
+public class Room implements Serializable {
+
+    private String id;
+    private String roomname;
+    private String lastMsg;
+
+
+    public Room(String id, String roomname, String lastMsg) {
+        this.id = id;
+        this.roomname = roomname;
+        this.lastMsg = lastMsg;
+    }
+
+    public Room(){}
+
+    public String getLastMsg() {
+        return lastMsg;
+    }
+
+    public void setLastMsg(String lastMsg) {
+        this.lastMsg = lastMsg;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRoomname() {
+        return roomname;
+    }
+
+    public void setRoomname(String roomname) {
+        this.roomname = roomname;
+    }
+}
+
